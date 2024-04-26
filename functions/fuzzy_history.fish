f7442a141104a328b3ece4104ab385aba387028d
kse201 <kse.201@gmail.com>
2024-04-26 16:30:00 +0900

    tweaks

--- functions/fuzzy_history.fish
+++ functions/fuzzy_history.fish
@@ -1,15 +1,7 @@
 function fuzzy_history
     set -l query (commandline)
 
-    if test -n $query
-        set query "$query"
-    end
-
     set prompt "HISTORY> "
-    if set -q $argv
-        history | sed "s/\r//" | fzf --prompt $prompt --query $query | read line
-    else
-        history | sed "s/\r//" | fzf --prompt $prompt --query $argv | read line
-    end
+    history | sed "s/\r//" | fzf --prompt $prompt --query $query | read line
     commandline $line
 end
